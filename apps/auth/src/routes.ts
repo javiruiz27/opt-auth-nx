18da6cfdbe48a4b9f37628891549eaad2553a885
Javier Ruiz <javiruizz.jr@gmail.com>
2024-12-17 09:12:14 +0100

    feat: valid otp auth

--- apps/auth/src/routes.ts
+++ apps/auth/src/routes.ts
@@ -1,35 +1,41 @@
-import koaRouter from "koa-router";
-import crypto from "crypto";
+import koaRouter from 'koa-router';
+import crypto from 'crypto';
 import jwt from 'jsonwebtoken';
 
 const router = new koaRouter();
 
-router.get("hello", "/", (ctx) => {
-    ctx.body = "<h1>Hello</h1>";
+router.get('hello', '/', (ctx) => {
+  ctx.body = '<h1>Hello</h1>';
 });
 
 router.post('/otp/generate', async (ctx, next) => {
-    const otp = crypto.randomBytes(2).toString('hex');
-    const email = ctx.request.body;
-    //set in context state temporally
-    ctx.state.users = email;
-    ctx.body = { otp };
+  const otp = crypto.randomBytes(2).toString('hex');
+  const email = ctx.request.body;
+
+  ctx.app.user = { email: email.email, otp };
+
+  ctx.body = { otp };
+
+  next();
 });
 
 router.post('/otp/verify', async (ctx, next) => {
-    const { otp: userOtp, email } = ctx.request.body;
-    
-    /*const storedOtp = ctx.state.users;  // retrieve stored OTP
-    console.log("🚀 ~ router.post ~ storedOtp:", storedOtp)*/
+  const { otp: userOtp, email } = ctx.request.body;
+  console.log('users', ctx.app.user);
 
-    /*if (userOtp === storedOtp) {
-      
-    } else {
-      ctx.status = 401;
-      ctx.body = { error: 'Invalid OTP' };
-    }*/
-      const token = jwt.sign({ user: email }, 'your-secret-key', { expiresIn: '1h' });
-      ctx.body = { token };
+  const storedOtp = ctx.app.user?.otp ?? ''; // retrieve stored OTP
+
+  if (userOtp === storedOtp) {
+    const token = jwt.sign({ user: email }, 'your-secret-key', {
+      expiresIn: '1h',
+    });
+    ctx.body = { token };
+  } else {
+    ctx.status = 401;
+    ctx.body = { error: 'Invalid OTP' };
+  }
+
+  next();
 });
 
 export default router;
